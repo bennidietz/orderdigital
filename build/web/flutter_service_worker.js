d9cfc36d1bf28605376a6b8839fab07817d0aa9d
Benni Dietz <b_diet05@uni-muenster.de>
2021-09-25 00:20:29 +0200

    New web built

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "fa8a5908c18730378a0bd12c29c93bd0",
-"index.html": "08e5ef3b496c1d6d7452d1f5c76a711a",
-"/": "08e5ef3b496c1d6d7452d1f5c76a711a",
+"index.html": "f65b165f19aae5bb6d8015d0ae9c3cb7",
+"/": "f65b165f19aae5bb6d8015d0ae9c3cb7",
 "main.dart.js": "065a957a9d2c7b75ede811a092ebff0e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
